54ebfa8bde7c3f433721a093b82f31daadc8d58f
Yousef Mohaned <youssief192678@fci.bu.edu.eg>
2025-09-28 21:00:47 +0300

    build project intializations

---
+++ final-project/src/main/java/org/algoriza/Main.java
@@ -0,0 +1,11 @@
+package org.algoriza;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger log = LoggerFactory.getLogger(Main.class);
+        log. info("Ensure Logging Work");
+    }
+}
---
+++ final-project/src/main/java/org/algoriza/pages/BasePage.java
@@ -0,0 +1,4 @@
+package org.algoriza.pages;
+
+public class BasePage {
+}
